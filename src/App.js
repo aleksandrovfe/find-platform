8e2848c355f1bb4665919a6ec90556979ef85318
aleksandrovfe <aleksandrov7296@gmail.com>
2020-05-18 20:44:37 +0300

    prepare start page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 const App = () => {
   return (
-    <div className="App">
-
+    <div className="application">
+      <h1 className="application__title">Application</h1>
     </div>
   );
 }
