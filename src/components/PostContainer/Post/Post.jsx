a073a3427c5bf34534ddb128e0cf40b7a2254d4b
Aleksandr Aleksandrov <47118920+aleksandrovfe@users.noreply.github.com>
2020-06-19 15:01:01 +0300

    add post page (#4)

---
+++ src/components/PostContainer/Post/Post.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {landingData} from "../../../data/landingData";
+
+export const Post = () => {
+    return (
+        <div className="post">
+            <div className="post__header">
+                <img className="post__teacher-image" src="https://images.pexels.com/photos/3623309/pexels-photo-3623309.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt=""/>
+                <div className="post__teacher-description">
+                    <div>
+                        <h2 className="post__teacher-name">{landingData[0].teacherName}</h2>
+                        <p className="post__teacher-subject">{landingData[0].title}</p>
+                        <button className="post__button">Message</button>
+                        <button className="post__button">Add to bookmarks</button>
+                    </div>
+                    <h4 className="teacher-price-wrapper">
+                        <p className="teacher-price">{landingData[0].price}$</p> per hour
+                    </h4>
+                </div>
+            </div>
+            <h3 className="post__subtitle">Description</h3>
+            <p className="post__description">{landingData[0].text}</p>
+        </div>
+    )
+}
